6fda88f7931352ce7d29d436287ee55f89091df3
Adrian Sroka <suvroc@o2.pl>
2016-12-27 13:56:13 +0100

    Project setup

---
+++ Nutrients.Api/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Nutrients.Api.Controllers
+{
+    public class TestController : ApiController
+    {
+        public int Test()
+        {
+            return 12;
+        }
+    }
+}
---
+++ Nutrients.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Nutrients.Api.WebApiApplication" Language="C#" %>
